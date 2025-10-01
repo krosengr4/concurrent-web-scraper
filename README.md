60795dd6b23ae3d2be262ecb4e8da9f6aeeb4032
Kevin Rosengren <rosenkev4@gmail.com>
2025-09-30 19:24:10 -0500

    Refined pseudocode

--- README.md
+++ README.md
@@ -13,3 +13,16 @@ The concurrent web scraper is a command line application that takes a list of UR
 - HTML Parsing: You'll need to parse the HTML to find the data tags that you wish to scrape. You can use Go's standard library or a third-partyt library like goquery.
 
 
+--- PROJECT STEPS ---
+
+Step 1: Read Input: The program should accept a list of URLs, either from the command line or from a text file.
+
+Step 2: Create a Worker: Write a function that takes a URL as input. This function will be the "worker" goroutine.
+
+Step 3: Fetch and Parse: Inside the worker function, use http.Get to fetch the HTML content. Then, parse the HTML to find the page's title.
+
+Step 4: Use Channels: Create a channel to send the results back to the main goroutine. Your worker function will send a struct containing the URL and its title to this channel.
+
+Step 5: Main Goroutine: The main function will launch a goroutine for each URL and then wait for all of them to finish by receiving the results from the channel.
+
+Step 6: Print Results: Finally, print the URL and its corresponding title in a clean, formatted way.
--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
-// --- PROJECT STEPS ---
+// todo: Create struct for ScrapeResult with URL Title and Error
 
-// Step 1: Read Input: The program should accept a list of URLs, either from the command line or from a text file.
+// todo: Create function for ScrapeWorker that takes in url(string) and results(writes to channel)
+// 1: Fetch the URL content (http.Get)
+// 2: Parse the HTML body (html.Parse)
+// 3: Extract the title (extractTitle func)
+// 4: Send the successful result back
 
-// Step 2: Create a Worker: Write a function that takes a URL as input. This function will be the "worker" goroutine.
+// todo: Create helper function to recursively traverse the HTML doc and find the title tag.
+// This will take in var of type *html.Node and return a string
+// for c := n.FirstChild; c != nil; c = c.NextSibling {
+// 	// Recursively call extractTitle on child nodes
+// 	if title := extractTitle(c); title != "" {
+// 		return title
+// 	}
+// }
+// return ""
 
-// Step 3: Fetch and Parse: Inside the worker function, use http.Get to fetch the HTML content. Then, parse the HTML to find the page's title.
-
-// Step 4: Use Channels: Create a channel to send the results back to the main goroutine. Your worker function will send a struct containing the URL and its title to this channel.
-
-// Step 5: Main Goroutine: The main function will launch a goroutine for each URL and then wait for all of them to finish by receiving the results from the channel.
-
-// Step 6: Print Results: Finally, print the URL and its corresponding title in a clean, formatted way.
+// todo: Main function
+// Get URL or list of URLS
+// Create buffered channel to hold the results, size = to number of URLS
+// For each URL, launch a worker goroutine
+//
